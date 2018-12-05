6367271a55e133e16d19cc49a75c5a2508e5c10f
Tajinder Chumber <chumbert94@gmail.com>
2018-12-05 21:25:22 +0000

    day03 part2

--- lib/day03/day03.ex
+++ lib/day03/day03.ex
@@ -1,6 +1,7 @@
 defmodule AdventOfCode2018.Day03 do
   alias AdventOfCode2018.Day03.Rectangle
 
+  # puzzle answer is 105071
   def part1() do
     claims = read_input()
 
@@ -8,9 +9,7 @@ defmodule AdventOfCode2018.Day03 do
 
     claims
     |> Enum.map(fn claim ->
-      for i <- 0..(claim.height - 1), j <- 0..(claim.width - 1) do
-        j + claim.delta_l + fabric_width * (claim.delta_t + i)
-      end
+      build_coords(claim, fabric_width)
     end)
     |> List.flatten()
     |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
@@ -23,12 +22,36 @@ defmodule AdventOfCode2018.Day03 do
     end)
   end
 
+  # puzzle answer is 222
+  # i know performance it's crap but i'm tired
+  def part2() do
+    claims = read_input()
+
+    {fabric_width, _fabric_height} = claims |> find_fabric_size()
+
+    claims
+    |> Enum.map(fn claim ->
+      {claim.id,
+       build_coords(claim, fabric_width)
+       |> MapSet.new()}
+    end)
+    |> Enum.map(fn {id, claim} ->
+      {id, coords_overlap?(claim, claims, fabric_width)}
+    end)
+    |> Enum.filter(fn {_, overlapping} -> overlapping == false end)
+  end
+
   defp read_input() do
     {:ok, input} = File.read("./lib/day03/input.txt")
 
     String.split(input, "\n")
     |> Enum.map(&String.split(&1, " "))
     |> Enum.map(fn x ->
+      id =
+        Enum.at(x, 0)
+        |> String.trim_leading("#")
+        |> String.to_integer()
+
       deltas =
         Enum.at(x, 2)
         |> String.split(",")
@@ -40,6 +63,7 @@ defmodule AdventOfCode2018.Day03 do
         |> Enum.map(&String.to_integer(&1))
 
       %Rectangle{
+        id: id,
         delta_l: Enum.at(deltas, 0),
         delta_t: Enum.at(deltas, 1),
         width: Enum.at(dimensions, 0),
@@ -70,5 +94,28 @@ defmodule AdventOfCode2018.Day03 do
       |> Enum.max()
 
     {max_delta_l + max_delta_width, max_delta_t + max_delta_height}
+  end
+
+  defp build_coords(claim, fabric_width) do
+    for i <- 0..(claim.height - 1), j <- 0..(claim.width - 1) do
+      j + claim.delta_l + fabric_width * (claim.delta_t + i)
+    end
+  end
+
+  defp coords_overlap?(claim_to_check, claims, fabric_width) do
+    Enum.map(claims, fn claim ->
+      claim_coords = build_coords(claim, fabric_width) |> MapSet.new()
+
+      MapSet.intersection(claim_coords, claim_to_check)
+      |> Enum.empty?() && !MapSet.equal?(claim_coords, claim_to_check)
+    end)
+    |> Enum.count(fn intersection -> !intersection end)
+    |> case do
+      1 ->
+        false
+
+      _ ->
+        true
+    end
   end
 end
